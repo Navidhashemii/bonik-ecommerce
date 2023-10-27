d97e55a482fe4b1b0d316dd29994c63c8f42b547
Navid Hashemi <nhashemi007@yahoo.com>
2023-10-27 11:18:02 +0330

    change database name

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,22 @@
 import { Box, Container } from "@mui/material";
 import TopBanners from "../components/TopBanners";
 import BenefitPart from "../components/BenefitPart";
+import { getLocalData } from '../../json/lib/localdata';
 
 
+export default async function Home() {
 
-export default function Home() {
+  const data = await getLocalData();
+  if (!data) {
+    throw new Error("Failed trying to get data");
+  }
+
+  
   return (
     <Container sx={{marginTop:"15px"}} >
 
       <TopBanners/>
       <BenefitPart/>
-
 
     </Container>
     
