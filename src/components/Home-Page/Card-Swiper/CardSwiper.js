356705b3d8610913b3df2eb1fcbf2aefa42ac325
Navid Hashemi <nhashemi007@yahoo.com>
2023-11-02 20:54:02 +0330

    add card icons and styles on hover and onclick

--- src/components/Home-Page/Card-Swiper/CardSwiper.js
+++ src/components/Home-Page/Card-Swiper/CardSwiper.js
@@ -1,23 +1,33 @@
 'use client'
 
-import { Box, Button, Divider, Typography } from "@mui/material";
+import { useState } from "react";
+import { Box, Button, Divider, Typography, IconButton } from "@mui/material";
 import Rating from '@mui/material/Rating';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Image from "next/image";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
+import styles from './CardSwiper.module.css'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import './CardSwiper.css'
-import Image from "next/image";
-import styles from './CardSwiper.module.css'
 
 
 
 function CardSwiper({data}) {
 
+    const [liked, setLiked] = useState(false)
+
+    const handleLike = () => setLiked(!liked)
+
+    const test = () => null
+
   return (
     <>
         <Swiper
-            modules={[ Navigation, ]}
+            modules={[ Navigation ]}
             className={styles.swiperStyles}
             spaceBetween={20}
             navigation={true}
@@ -30,9 +40,13 @@ function CardSwiper({data}) {
                             alt={name}         
                             width={200}
                             height={270}
+                            className={styles.cardImage}
                         />
                         <Divider variant="middle"/>
-                        <Typography variant="subtitle1" className={styles.cardName}>
+                        <Typography
+                            variant="subtitle1"
+                            className={styles.cardName}
+                        >
                             {name}
                         </Typography>
 
@@ -41,22 +55,35 @@ function CardSwiper({data}) {
                         ? 
 
                         <Box>
-                            <Box bgcolor="secondary.main" className={styles.discountBox}>
+                            <Box
+                                bgcolor="secondary.main"
+                                className={styles.discountBox}
+                            >
                                 {discount}%
                             </Box>
                                 
-                            <Typography variant="h6" color="secondary" className={styles.afterDiscount}>
+                            <Typography
+                                variant="h6"
+                                color="secondary"
+                                className={styles.afterDiscount}
+                            >
                                 ${Math.floor(price - ((price * discount) / 100)).toFixed(2)}
                             </Typography>
 
-                            <Typography variant="subtitle1" className={styles.beforeDiscount}>
+                            <Typography
+                                variant="subtitle1"
+                                className={styles.beforeDiscount}
+                            >
                                 ${price.toFixed(2)}
                             </Typography>
                         </Box>
 
                         : 
 
-                        <Typography variant="h6" className={styles.price}>
+                        <Typography
+                            variant="h6"
+                            className={styles.price}
+                        >
                             {price}
                         </Typography>
 
@@ -64,7 +91,10 @@ function CardSwiper({data}) {
 
                         <Box className={styles.ratingContainer}>
                             <Rating size="small" value={star}/>
-                            <Typography variant="subtitle2" className={styles.rateCount}>
+                            <Typography
+                                variant="subtitle2"
+                                className={styles.rateCount}
+                            >
                                 ({rate})
                             </Typography>
                         </Box>
@@ -72,6 +102,20 @@ function CardSwiper({data}) {
                         <Button className={styles.button}>
                             Add To Cart
                         </Button>
+                        <IconButton onClick={test}  className={styles.icon1}>
+                            <VisibilityOutlinedIcon/>
+                        </IconButton>
+                        
+                        {liked
+                            ? 
+                            <IconButton  onClick={handleLike} className={styles.icon2}>
+                                <FavoriteIcon/>
+                            </IconButton>
+                            :
+                            <IconButton onClick={handleLike} className={styles.icon2}>
+                                <FavoriteBorderOutlinedIcon />
+                            </IconButton>
+                        }
                     </Box>
                 </SwiperSlide>
             ))}
