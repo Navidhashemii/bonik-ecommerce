7bc0bd49f36575095b1923749332919e5a874798
Navid Hashemi <nhashemi007@yahoo.com>
2023-11-03 20:14:00 +0330

    add display flex to card swiper

--- src/components/Home-Page/Card-Swiper/CardSwiper.js
+++ src/components/Home-Page/Card-Swiper/CardSwiper.js
@@ -43,68 +43,71 @@ function CardSwiper({data}) {
             {data.map(({id, image1, name, price, star, rate, discount}) => (
                 <SwiperSlide key={id}>
                     <Box className={styles.cardContainer}>
-                        <Image
-                            src={image1}
-                            alt={name}         
-                            width={200}
-                            height={270}
-                            className={styles.cardImage}
-                        />
-                        <Divider variant="middle"/>
-                        <Typography
-                            variant="subtitle1"
-                            className={styles.cardName}
-                        >
-                            {name}
-                        </Typography>
-
-                        {discount 
-
-                        ? 
-
                         <Box>
-                            <Box
-                                bgcolor="secondary.main"
-                                className={styles.discountBox}
-                            >
-                                {discount}%
-                            </Box>
-                                
-                            <Typography
-                                variant="h6"
-                                color="secondary"
-                                className={styles.afterDiscount}
-                            >
-                                ${Math.floor(price - ((price * discount) / 100)).toFixed(2)}
-                            </Typography>
-
+                            <Image
+                                src={image1}
+                                alt={name}         
+                                width={200}
+                                height={270}
+                                className={styles.cardImage}
+                            />
+                            <Divider variant="middle"/>
                             <Typography
                                 variant="subtitle1"
-                                className={styles.beforeDiscount}
+                                className={styles.cardName}
+                            >
+                                {name}
+                            </Typography>
+                        </Box>
+                        <Box>
+                            {discount 
+
+                            ? 
+
+                            <Box>
+                                <Box
+                                    bgcolor="secondary.main"
+                                    className={styles.discountBox}
+                                >
+                                    {discount}%
+                                </Box>
+                                    
+                                <Typography
+                                    variant="h6"
+                                    color="secondary"
+                                    className={styles.afterDiscount}
+                                >
+                                    ${Math.floor(price - ((price * discount) / 100)).toFixed(2)}
+                                </Typography>
+
+                                <Typography
+                                    variant="subtitle1"
+                                    className={styles.beforeDiscount}
+                                >
+                                    ${price.toFixed(2)}
+                                </Typography>
+                            </Box>
+
+                            : 
+
+                            <Typography
+                                variant="h6"
+                                className={styles.price}
                             >
                                 ${price.toFixed(2)}
                             </Typography>
-                        </Box>
 
-                        : 
+                            }
 
-                        <Typography
-                            variant="h6"
-                            className={styles.price}
-                        >
-                            ${price.toFixed(2)}
-                        </Typography>
-
-                        }
-
-                        <Box className={styles.ratingContainer}>
-                            <Rating size="small" value={star}/>
-                            <Typography
-                                variant="subtitle2"
-                                className={styles.rateCount}
-                            >
-                                ({rate})
-                            </Typography>
+                            <Box className={styles.ratingContainer}>
+                                <Rating size="small" value={star}/>
+                                <Typography
+                                    variant="subtitle2"
+                                    className={styles.rateCount}
+                                >
+                                    ({rate})
+                                </Typography>
+                            </Box>
                         </Box>
 
                         <Button className={styles.button}>
