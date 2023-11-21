0d5a478b55fabc975890d4883d766d83b8680e47
Navid Hashemi <nhashemi007@yahoo.com>
2023-11-22 00:37:04 +0330

    refactor airport banner to more readable one

--- src/components/Home-Page/Airport-Banner/AirportBanner.js
+++ src/components/Home-Page/Airport-Banner/AirportBanner.js
@@ -4,35 +4,22 @@ import styles from './AirportBanner.module.css'
 function AirportBanner() {
   return (
     <Box className={styles.boxContainer}>
-
-        <Box className={styles.titleBox}>
-              <Typography
-                variant='h3'
-                className={styles.title}
-              >
-                BLACK FRIDAY SALE!
-              </Typography>
-            <Box className={styles.skewBox}/>
+      <Box className={styles.titleBox}>
+        <Typography variant='h3' className={styles.title}>
+          BLACK FRIDAY SALE!
+        </Typography>
+        <Box className={styles.skewBox} />
+      </Box>
+      <Box className={styles.marquee}>
+        <Box className={styles.marqueeContainer}>
+          <Typography variant='h4' className={styles.marqueeText}>
+            PAY ONLY HALF PRICE FOR YOUR LOVING CLOTHES
+          </Typography>
         </Box>
-
-        <Box className={styles.marquee}>
-            <Box className={styles.marqueeContainer}>
-                <Typography
-                  variant='h4'
-                  className={styles.marqueeText}
-                >
-                  PAY ONLY HALF PRICE FOR YOUR LOVING CLOTHES
-                </Typography>
-            </Box>
-        </Box>
-
-        <Button
-          variant='contained'
-          className={styles.button}
-        >
-          Shop Now
-        </Button>
-
+      </Box>
+      <Button variant='contained' className={styles.button}>
+        Shop Now
+      </Button>
     </Box>
   )
 }
